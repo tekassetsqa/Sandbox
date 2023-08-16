f5afc543f92b8a62b24a47b24c60313bf3dc5f8e
Sai Vaddella <Sai.Vaddella@jtax.com>
2023-08-15 21:20:11 -0400

    Launching Browser

---
+++ AmarTestProject/AmarTestProject/StepDefinitions/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmarTestProject.StepDefinitions
+{
+    public class Class1
+    {
+        [Test]
+        public void Method1() {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com");
+        }
+    }
+}
